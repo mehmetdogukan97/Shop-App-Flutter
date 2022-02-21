838ab998f301aa4c197338341ee6e8921ca8afb9
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-02-21 14:27:59 +0300

    Gridview structure is created of Products overview screen.

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -43,6 +43,17 @@ class ProductsOverviewScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('MyShop'),
       ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => Container(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10
+        ),
+      ),
     );
   }
 }
