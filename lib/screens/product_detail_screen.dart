a98f2ea20e022cb2bb6dbdfadcfa24ab08c54628
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-02-21 15:44:54 +0300

    working on navigation and passing the data

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
-class ProductDetailScreen extends StatelessWidget {
 
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  //
+  // ProductDetailScreen(this.title);
+  static const routeName='/product-detail';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final productId = ModalRoute.of(context).settings.arguments as String; // is id
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -12,9 +13,17 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
@@ -29,7 +38,7 @@ class ProductItem extends StatelessWidget {
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
-            onPressed: (){},
+            onPressed: () {},
             color: Theme.of(context).accentColor,
           ),
         ),
