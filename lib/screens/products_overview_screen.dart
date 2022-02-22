35db87af01a75fe7787a4bcf2f41e32405c4ccbb
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-02-22 15:32:38 +0300

    Made refactoring of filter code

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
 import '../widgets/products_grid.dart';
-import 'package:provider/provider.dart';
-import '../providers/products.dart';
-enum FiltersOptions{
-  Favorites,
-  All
+
+enum FiltersOptions { Favorites, All }
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
 }
 
-class ProductsOverviewScreen extends StatelessWidget {
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
-    final productsContainer = Provider.of<Products>(context,listen: false);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
         actions: [
           PopupMenuButton(
             icon: Icon(Icons.more_vert),
-            onSelected: (FiltersOptions  selectedItem) {
-              if(selectedItem == FiltersOptions.Favorites){
-                  productsContainer.showFavoritesOnly();
-              }else{
-                  productsContainer.showAll();
-              }
+            onSelected: (FiltersOptions selectedItem) {
+              setState(() {
+                if (selectedItem == FiltersOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
             },
             itemBuilder: (_) => [
               PopupMenuItem(
@@ -34,12 +37,11 @@ class ProductsOverviewScreen extends StatelessWidget {
                 child: Text('Show All'),
                 value: FiltersOptions.All,
               ),
-
             ],
           )
         ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -4,11 +4,14 @@ import '../providers/products.dart';
 import '../widgets/product_item.dart';
 class ProductsGrid extends StatelessWidget {
 
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
 
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
