e58ddf10b56ffc62dedfffd6764f1fdfc3c11ef2
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-02-22 14:55:12 +0300

    Working on pop menu

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
- import '../widgets/products_grid.dart';
-class ProductsOverviewScreen extends StatelessWidget {
+import '../widgets/products_grid.dart';
 
+enum FiltersOptions{
+  Favorites,
+  All
+}
+
+class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            onSelected: (FiltersOptions  selectedItem) {
+              if(selectedItem == FiltersOptions.Favorites){
+
+              }else{
+
+              }
+            },
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FiltersOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FiltersOptions.All,
+              ),
+
+            ],
+          )
+        ],
       ),
       body: ProductsGrid(),
     );
   }
 }
-
