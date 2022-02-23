ab12cf0ff69eea2d68c3dbefcda74600542a882c
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-02-23 11:30:57 +0300

    shopping cart icon and badget  integration is completedon the app bar

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -15,10 +15,15 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  Map<String, CartItem> _items;
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
+  }
+
+  int get itemCount{
+    return _items.length;
+
   }
 
   void addItem(String productId, double price, String title) {
@@ -41,5 +46,6 @@ class Cart with ChangeNotifier {
                 quantity: 1,
               ));
     }
+    notifyListeners();
   }
 }
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
 import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
 
 enum FiltersOptions { Favorites, All }
 
@@ -13,6 +16,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
@@ -38,7 +42,16 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                 value: FiltersOptions.All,
               ),
             ],
-          )
+          ),
+          Consumer<Cart>(builder: (_,cart,ch)=>Badge(
+            child: ch,
+            value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: (){ },
+            ),
+          ),
         ],
       ),
       body: ProductsGrid(_showOnlyFavorites),
