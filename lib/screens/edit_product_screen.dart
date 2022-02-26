a6a071c8bf37d14df8638573a3b8d3f1842069ae
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-02-26 14:08:30 +0300

    Add product method is written and intagrate with edit product screen

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../providers/product.dart';
-
+import '../providers/products.dart';
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
 
@@ -54,10 +55,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
       return;
     }
     _form.currentState.save();
-    print(_editedProduct.title);
-    print(_editedProduct.imageUrl);
-    print(_editedProduct.price);
-    print(_editedProduct.description);
+
+    Provider.of<Products>(context,listen: false).addProduct(_editedProduct);
+    Navigator.of(context).pop();
+
   }
 
   @override
