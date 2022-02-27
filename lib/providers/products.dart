dba3fb37e40e4d8b20bf315ef9b2d7cce944debf
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-02-27 14:43:21 +0300

    Add product is integrated with firebase in products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import './product.dart';
-
+import 'package:http/http.dart' as http;
 class Products with ChangeNotifier {
   List<Product> _items = [
     Product(
@@ -65,16 +66,27 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
-    final newProduct = Product(
-      id: DateTime.now().toString(),
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-    );
-    _items.add(newProduct);
+    final url = Uri.https('flutter-update-3129e-default-rtdb.firebaseio.com','/products.json');
+    http.post(url,body: json.encode({//backend sending
+      'title': product.title,
+      'description': product.description,
+      'price': product.price,
+      'imageUrl': product.imageUrl,
+      'isFavorite':product.isFavorite,
 
-    notifyListeners();
+    }),).then((response) {
+      final newProduct = Product(//fronted sending
+        id: json.decode(response.body)['name'],
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+      );
+      _items.add(newProduct);
+      notifyListeners();
+
+    });
+
   }
 
   void updateProduct(String id, Product newProduct){
