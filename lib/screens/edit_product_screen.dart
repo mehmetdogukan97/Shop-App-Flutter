396f86d3d190bc54e1732de7cf99dab3c9ad7007
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-02-28 13:55:06 +0300

    update data part is connected with firebase

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -91,12 +91,9 @@ class _EditProductScreenState extends State<EditProductScreen> {
     });
 
     if (_editedProduct.id != null) {
-      Provider.of<Products>(context, listen: false)
+      await Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-      Navigator.of(context).pop();
-      setState(() {
-        _isLoading = false;
-      });
+
     } else {
       try {
         await Provider.of<Products>(context, listen: false)
@@ -117,13 +114,19 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-        Navigator.of(context).pop();
-      }
+       }
+       // finally {
+      //   setState(() {
+      //     _isLoading = false;
+      //   });
+      //   Navigator.of(context).pop();
+      // }
     }
+    setState(() {
+      _isLoading = false;
+    });
+    Navigator.of(context).pop();
+
   }
 
   @override
