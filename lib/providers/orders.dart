e6ccad4199769e47f4985039e33f99b2646f21a7
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-02-28 16:29:03 +0300

    Fetching orders data and fix an issue

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -24,20 +24,49 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  Future<void> addOrder(List<CartItem> cartProducts, double total) async{
+  Future<void> fetchAndSetOrders() async {
+    final url = Uri.https(
+        'flutter-update-3129e-default-rtdb.firebaseio.com', '/orders.json');
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if(extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          products: (orderData['products'] as List<dynamic>)
+              .map((item) => CartItem(
+                  id: item['id'],
+                  title: item['title'],
+                  quantity: item['quantity'],
+                  price: item['price']))
+              .toList(),
+          dateTime: DateTime.parse(orderData['dateTime'])));
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final url = Uri.https(
         'flutter-update-3129e-default-rtdb.firebaseio.com', '/orders.json');
     final timeStamp = DateTime.now();
-    final response = await http.post(url,body: json.encode({
-      'amount':total,
-      'dateTime': timeStamp.toIso8601String(),
-      'products' : cartProducts.map((cp) => {
-        'id' : cp.id,
-        'title':cp.title,
-        'quantity':cp.quantity,
-        'price':cp.price,
-      }).toList(),
-    }));
+    final response = await http.post(url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timeStamp.toIso8601String(),
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
+        }));
     _orders.insert(
       0,
       OrderItem(
