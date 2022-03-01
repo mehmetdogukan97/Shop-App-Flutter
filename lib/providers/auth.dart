92e895462efca45358287f51d5a031c37cf5dff9
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-03-01 16:06:57 +0300

    Showing error is prapared.

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,25 +1,34 @@
 import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
-
+import '../models/http_exception.dart';
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> _authenticate(String email,String password,String urlSegment) async {
+  Future<void> _authenticate(String email, String password,
+      String urlSegment) async {
     final url = Uri.parse(
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBFMVA9urOMMNz6N30Wpq01pq26dmJb1XI');
-    final response = http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if(responseData['error'] != null){
+        throw HttpException(responseData['error']['message']);
+      }
+    }catch(error){
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
